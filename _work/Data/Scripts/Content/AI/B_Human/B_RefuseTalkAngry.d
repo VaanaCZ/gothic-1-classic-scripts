656df37c00d3b356b07454003e7811d2ad96e720
VaanaCZ <vaana@centrum.cz>
2022-10-24 17:27:20 +0200

    Removed files

--- _work/Data/Scripts/Content/AI/B_Human/B_RefuseTalkAngry.d
+++
@@ -1,5 +0,0 @@
-func void B_RefuseTalkAngry ()
-{
-	PrintDebugNpc( PD_ZS_FRAME, "B_RefuseTalkAngry" );
-	B_Say 			( self, other, "$NOTNOW");
-};
--- _work/Data/Scripts/Content/AI/B_Human/B_TossAndTurn.d
+++
@@ -1,8 +0,0 @@
-func void B_TossAndTurn ()
-{
-	PrintDebugNpc		(PD_ZS_FRAME, "B_TossAndTurn" );
-
-	Npc_ClearAIQueue	(self);
-	AI_PlayAni			(self,	"R_BED_RANDOM_2");
-};
-	
