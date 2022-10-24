55cf01b3846263c0139f36e8b72414c6200222b8
VaanaCZ <vaana@centrum.cz>
2022-10-24 16:31:57 +0200

    Removed files

--- _work/Data/Scripts/content/Story/EVENTS/EventsOldmine.d
+++
@@ -1,8 +0,0 @@
-FUNC VOID EVENT_OM_CRAWLER_01 ()
-{
-	Wld_InsertNpc		(MINECRAWLER,"OM_CAVE3_28");
-	Wld_InsertNpc		(MINECRAWLER,"OM_CAVE3_28");
-	Wld_InsertNpc		(MINECRAWLER,"OM_059");
-	Wld_InsertNpc		(MINECRAWLER,"OM_059");
-	Wld_InsertNpc		(MINECRAWLER,"OM_060");
-};
